016d95baa6393acb161f64062425291a29badeba
juyeon0815 <wndusx1@gmail.com>
2021-10-10 19:45:05 +0900

    Rename : 11727

--- BOJ/src/dp/Main_BJ_11727_2xn타일링2.java
+++ BOJ/src/dp/Main_BJ_11727_2xn타일링2.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_BJ_2xn타일링2 {
+public class Main_BJ_11727_2xn타일링2 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
