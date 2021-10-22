c3b0fdee1d7feff08298f06ad05daa0a67aab584
juyeon0815 <wndusx1@gmail.com>
2021-10-22 21:06:18 +0900

    11399 ATM greedy

---
+++ BOJ/src/greedy/Main_BJ_11399_ATM.java
@@ -0,0 +1,34 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_BJ_11399_ATM {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int [] time = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) time[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(time);
+		
+		int sum=0, result=0;
+		for(int i=0;i<N;i++) {
+			sum = sum+time[i];
+			result += sum;
+		}
+		
+		System.out.println(result);
+
+	}
+
+}
