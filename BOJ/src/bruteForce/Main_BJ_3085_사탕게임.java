d2225278172dd7321d26cda940a3abd328304873
juyeon0815 <wndusx1@gmail.com>
2021-11-18 01:41:18 +0900

    3085 사탕 게임 bruteForce

---
+++ BOJ/src/bruteForce/Main_BJ_3085_사탕게임.java
@@ -0,0 +1,92 @@
+package bruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_BJ_3085_사탕게임 {
+	
+	static int [] dx = {-1,0,1,0};
+	static int [] dy = {0,1,0,-1};
+	
+	static int N, max=0;
+	static char [][] map;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		map = new char[N][N];
+		
+		for(int i=0;i<N;i++) {
+			String line = br.readLine();
+			for(int j=0;j<N;j++) {
+				map[i][j] = line.charAt(j);
+			}
+		}
+		
+		candy();
+		System.out.println(max);
+	}
+	
+	public static void candy() {
+		for(int x=0;x<N;x++) {
+			for(int y=0;y<N;y++) {
+				
+				for(int d=0;d<4;d++) {
+					int nx = x + dx[d];
+					int ny = y + dy[d];
+					
+					if(range(nx,ny) && map[x][y]!=map[nx][ny]) {
+						char temp = map[x][y];
+						map[x][y] = map[nx][ny];
+						map[nx][ny] = temp;
+						
+						check();
+						
+						temp = map[x][y];
+						map[x][y] = map[nx][ny];
+						map[nx][ny] = temp;
+					}
+				}
+			}
+		}
+	}
+	
+	public static boolean range(int x, int y) {
+		return x>=0 && x<N && y>=0 && y<N;
+	}
+	
+	public static void check() {
+		
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				count(i,j);
+			}
+		}	
+	}
+	
+	public static void count(int x, int y) {
+		
+		int yy=y, xx=x, row=1, col=1;
+		//연속된 행
+		for(int i=y+1;i<N;i++) {
+			if(map[x][yy]==map[x][i]) {
+				row++;
+				yy = i; 
+			}else break;
+		}
+		
+		for(int i=x+1;i<N;i++) {
+			if(map[xx][y]==map[i][y]) {
+				col++;
+				xx = i;
+			}else break;
+		}
+		
+		int result = Math.max(row, col);
+		max = Math.max(result, max);
+	}
+
+}
