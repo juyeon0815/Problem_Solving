1f76d064b1e7a7ee95ef946eabdf9ba1bb11257a
juyeon0815 <wndusx1@gmail.com>
2021-12-19 13:18:50 +0900

    1182 부분수열의 합 backTracking

---
+++ BOJ/src/backTracking/Main_BJ_1182_부분수열의합.java
@@ -0,0 +1,43 @@
+package backTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ_1182_부분수열의합 {
+	
+	static int [] arr;
+	static int N,S, answer;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		subset(0,0);
+		System.out.println(S==0?answer-1:answer);
+	}
+	
+	public static void subset(int cnt, int num) {
+		if(cnt==N) {
+			if(num==S) answer++;
+			return;
+		}
+		
+		subset(cnt+1,num+arr[cnt]);
+		subset(cnt+1,num);
+	}
+
+}
